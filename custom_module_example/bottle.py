b9af4a4b397e91da91f3fab8a7bdb4369410431c
April Carter <april@cortexgraphs.com>
2018-05-19 00:09:29 -0400

    Custom module example

---
+++ custom_module_example/bottle.py
@@ -0,0 +1,7 @@
+from water import water
+
+def bottle():
+    print(f"I am a bottle, and my __name__ is \"{__name__}\"")
+
+bottle()
+water()
---
+++ custom_module_example/water.py
@@ -0,0 +1,5 @@
+def water():
+    print(f"I am water in a bottle. My __name__ is \"{__name__}\"")
+
+if __name__ == "__main__":
+    water()
