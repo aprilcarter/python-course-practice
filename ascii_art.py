47ae1f877055b6d0e8c8ad345e60f4c272b6c257
April Carter <april@cortexgraphs.com>
2018-05-19 19:56:28 -0400

    Beautify ascii_art.py

--- ascii_art.py
+++ ascii_art.py
@@ -1,6 +1,7 @@
 from pyfiglet import figlet_format
 from termcolor import colored
 
+
 def print_art(text, color):
     valid_colors = ("red", "green", "blue", "magenta", "cyan")
     if color.lower() not in valid_colors:
@@ -8,6 +9,7 @@ def print_art(text, color):
     ascii_art = figlet_format(text)
     colored_ascii = colored(ascii_art, color=color)
     print(colored_ascii)
+
 
 text = input("What would you like to write? ")
 color = input("What color would you like it to be? ")
