9b57480d84c2b1dea6c38eb58bc47769aeb88ce5
April Carter <april@cortexgraphs.com>
2018-06-01 23:48:14 -0400

    build a couple classes to spec assignment

---
+++ OOP_Examples/Cards/card.py
@@ -0,0 +1,7 @@
+class Card:
+    def __init__(self, value, suit):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
---
+++ OOP_Examples/Cards/deck.py
@@ -0,0 +1,41 @@
+from random import shuffle
+from card import Card
+
+
+class Deck:
+    def __init__(self):
+        values = ["A", "2", "3", "4", "5", "6",
+                  "7", "8", "9", "10", "J", "Q", "K"]
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        self.cards = [Card(v, s) for s in suits for v in values]
+
+    def __repr__(self):
+        return f"Deck of {self.count()} cards"
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, num):
+        count = self.count()
+
+        if count == 0:
+            raise ValueError("All cards have been dealt")
+
+        deal_count = min([count, num])
+        selection = self.cards[:deal_count]
+
+        del self.cards[:deal_count]
+        return selection
+
+    def shuffle(self):
+        if len(self.cards) < 52:
+            raise ValueError("Only full decks can be shuffled")
+            return
+        shuffle(self.cards)
+        return self
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self, num):
+        return self._deal(num)
