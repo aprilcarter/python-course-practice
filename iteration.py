9211f58351f130ad5afde0dacef7ec96c3e8f7b6
April Carter <april@cortexgraphs.com>
2018-06-07 23:30:12 -0400

    Generators

--- iteration.py
+++ iteration.py
@@ -31,3 +31,33 @@ class Counter:
 
 # for x in Counter(0, 10):
 #    print(x)
+
+
+# This generator function does half of what the Counter class does
+def count_up_to(max):
+    count = 1
+    while count <= max:
+        yield count
+        count += 1
+
+# counter = count_up_to(5)
+# next(counter)
+# for num in counter:
+#   print(num)
+
+
+# infinite generator
+def current_beat():
+    nums = (1, 2, 3, 4)
+    i = 0
+    while True:
+        if i >= len(nums):
+            i = 0
+        yield nums[i]
+        i += 1
+
+
+# Generator expressions work a bit like list comprehension but with ()
+generator = (num for num in range(1, 10))
+sum([num for num in range(1, 10)])  # creates and passes in a list
+sum(num for num in range(1, 10))  # uses generator expression. Better on memory
